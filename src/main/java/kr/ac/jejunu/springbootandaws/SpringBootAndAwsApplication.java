d05e4cfd0c14a82150b09525c44c1425580160a9
ksg0605 <ksg0605@gmail.com>
2022-06-24 00:58:40 +0900

    initial commit

---
+++ src/main/java/kr/ac/jejunu/springbootandaws/SpringBootAndAwsApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.springbootandaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAndAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/jejunu/springbootandaws/SpringBootAndAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.springbootandaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
