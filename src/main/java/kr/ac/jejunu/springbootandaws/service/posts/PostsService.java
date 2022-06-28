bc6b4ba2ea9e81da3631078df09ceacec3334df9
ksg0605 <ksg0605@gmail.com>
2022-06-28 22:48:14 +0900

    add controller

--- src/main/java/kr/ac/jejunu/springbootandaws/service/posts/PostsService.java
+++ src/main/java/kr/ac/jejunu/springbootandaws/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package kr.ac.jejunu.springbootandaws.service.posts;
 
 import kr.ac.jejunu.springbootandaws.domain.posts.Posts;
 import kr.ac.jejunu.springbootandaws.domain.posts.PostsRepository;
+import kr.ac.jejunu.springbootandaws.web.dto.PostsListResponseDto;
 import kr.ac.jejunu.springbootandaws.web.dto.PostsResponseDto;
 import kr.ac.jejunu.springbootandaws.web.dto.PostsSaveRequestDto;
 import kr.ac.jejunu.springbootandaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +32,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Object findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
---
+++ src/main/java/kr/ac/jejunu/springbootandaws/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package kr.ac.jejunu.springbootandaws.web.dto;
+
+import kr.ac.jejunu.springbootandaws.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
